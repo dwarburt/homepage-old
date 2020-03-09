c223c7fdabaf4db626bad33e8b88c5b857722f5e
David Warburton <david.warburton@gmail.com>
2020-03-09 12:21:57 -0400

    Adds terminal code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { TerminalService } from 'primeng/terminal/public_api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TerminalService]
 })
 export class AppComponent {
   title = 'app works!';
+  constructor(private terminalService : TerminalService) {
+    this.terminalService.commandHandler.subscribe(command => {
+      let response = (command === 'date') ? new Date().toDateString() : 'Unknown command: ' + command;
+      this.terminalService.sendResponse(response);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import {TerminalModule} from 'primeng/terminal';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    TerminalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
