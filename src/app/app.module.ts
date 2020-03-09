7b8049e6f81b0ad72db731dffb80440de358d25b
David Warburton <david.warburton@gmail.com>
2020-03-09 12:25:24 -0400

    update to newer angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import {TerminalModule} from 'primeng/terminal';
 
 @NgModule({
   declarations: [
@@ -13,9 +10,7 @@ import {TerminalModule} from 'primeng/terminal';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    TerminalModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
