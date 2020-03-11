dfee977bad6c46e4bd673153428b6c3ba869e47d
David Warburton <david.warburton@gmail.com>
2020-03-11 10:53:10 -0400

    doctl auth init

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@ set -e
 test -n "$GITHUB_SHA"
 test -n "$DOCTL_API_KEY"
 
+doctl auth init -t $DOCTL_API_KEY
+
 doctl \
-    -t $DOCTL_API_KEY \
     kubernetes cluster \
     config save rcluster
 echo "Deploying dwarburton/homepage:$GITHUB_SHA"
